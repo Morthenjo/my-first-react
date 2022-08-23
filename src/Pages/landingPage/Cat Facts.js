2881fd891ee4028f5e7f60552edac9ade3c3ef62
MorthenJo <MorthenJo.kodehode@gmail.com>
2022-08-23 14:35:42 +0200

    cat facts

--- src/Pages/landingPage/Cat Facts.js
+++ src/Pages/landingPage/Cat Facts.js
@@ -1,7 +1,34 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export default function CatFacts() {
+  const setApiError = "No pussies found";
+  const [data, setData] = useState([]);
+  const getData = () => {
+    axios
+      .get("https://catfact.ninja/facts?page=2")
+      .then((res) => {
+        setData(res.data.data);
+      })
+      .catch((res) => {
+        setApiError(res);
+      });
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  if (data) {
+    console.log(data);
+  }
+
   return (
     <div>
       <h1>Here comes the pussies</h1>
+      {data.map((item, idx) => (
+        <p key={idx}>{item.fact}</p>
+      ))}
     </div>
   );
 }
