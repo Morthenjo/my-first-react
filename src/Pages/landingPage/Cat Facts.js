2d7ab3fdd829784f70cb5ff9ac593cedcd8ddb6b
MorthenJo <MorthenJo.kodehode@gmail.com>
2022-08-24 10:21:44 +0200

    pussy

--- src/Pages/landingPage/Cat Facts.js
+++ src/Pages/landingPage/Cat Facts.js
@@ -2,33 +2,44 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 export default function CatFacts() {
-  const setApiError = "No pussies found";
-  const [data, setData] = useState([]);
-  const getData = () => {
-    axios
-      .get("https://catfact.ninja/facts?page=2")
-      .then((res) => {
-        setData(res.data.data);
-      })
-      .catch((res) => {
-        setApiError(res);
-      });
-  };
+  const [apiError, setApiError] = useState();
+  const [data, setData] = useState();
 
+  // eslint-disable-next-line
   useEffect(() => {
+    const getData = () => {
+      axios
+        .get("https://catfact.ninja/facts?page=2")
+        .then((res) => {
+          setData(res.data.data);
+        })
+        .catch((res) => {
+          setApiError(res);
+        });
+    };
     getData();
   }, []);
 
   if (data) {
-    console.log(data);
+    return (
+      <div>
+        <h1>Pussy facts</h1>
+        {data.map((item, idx) => (
+          <p key={idx}>{item.fact}</p>
+        ))}
+      </div>
+    );
+  } else if (apiError) {
+    return (
+      <div>
+        <h1>Failed to load pussy, please try again.</h1>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h1>Here comes the pussies</h1>
+      </div>
+    );
   }
-
-  return (
-    <div>
-      <h1>Here comes the pussies</h1>
-      {data.map((item, idx) => (
-        <p key={idx}>{item.fact}</p>
-      ))}
-    </div>
-  );
 }
